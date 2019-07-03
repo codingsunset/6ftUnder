79ddcbdeaf32a594517195231751f6226f4587d4
Moh Chica <onibadesh@yahoo.com>
2019-07-03 14:06:24 -0500

    css modified

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,13 @@
-import React from "react";
+ import React from "react";
 
-function Nav() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        6ftUnder - household compost logging and viewing app
-      </a>
-    </nav>
-  );
-}
+ function Nav() {
+   return (
+     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+       <a className="navbar-brand" href="/">
+         6ftUnder - household compost logging and viewing app
+       </a>
+     </nav>
+   );
+ }
 
-export default Nav;
+ export default Nav;
