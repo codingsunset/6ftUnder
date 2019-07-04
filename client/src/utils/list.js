3835244b0bc1e961c9c07919576eead4e4a73789
denise <46205053+denisezou@users.noreply.github.com>
2019-07-03 21:47:11 -0500

    update list of comprehensive veggies and fruits

--- client/src/utils/list.js
+++ client/src/utils/list.js
@@ -1,13 +1,273 @@
 const fruitsVeggies = [
-    {
-      name: "carrot",
-      weight: 1.2
-    },
-    {
-      name: "apple",
-      weight: 3.4
-    },
-    
-  ];
-  
+  {
+    name: "carrot",
+    weight: 1.2
+  },
+  {
+    name: "daikon",
+    weight: 1.2
+  },
+  {
+    name: "endive",
+    weight: 1.2
+  },
+  {
+    name: "eggplant",
+    weight: 1.2
+  },
+  {
+    name: "fig",
+    weight: 1.2
+  },
+  {
+    name: "fennel",
+    weight: 1.2
+  },
+  {
+    name: "honeydew",
+    weight: 1.2
+  },
+  {
+    name: "iceberg lettuce",
+    weight: 1.2
+  },
+  {
+    name: "rhubarb",
+    weight: 1.2
+  },
+  {
+    name: "radish",
+    weight: 1.2
+  },
+  {
+    name: "raisin",
+    weight: 1.2
+  },
+  {
+    name: "satsuma",
+    weight: 1.2
+  },
+  {
+    name: "apple",
+    weight: 3.4
+  },
+  {
+    name: "watermelon",
+    weight: 3.4
+  },
+  {
+    name: "pepper",
+    weight: 3.4
+  },
+  {
+    name: "olive",
+    weight: 3.4
+  },
+  {
+    name: "asperagus",
+    weight: 3.4
+  },
+  {
+    name: "broccoli",
+    weight: 3.4
+  },
+  {
+    name: "cabbage",
+    weight: 3.4
+  },
+  {
+    name: "corn on cob",
+    weight: 3.4
+  },
+  {
+    name: "cucumber",
+    weight: 3.4
+  },
+  {
+    name: "celery",
+    weight: 3.4
+  },
+  {
+    name: "spinach",
+    weight: 3.4
+  },
+  {
+    name: "lemon",
+    weight: 3.4
+  },
+  {
+    name: "green bean",
+    weight: 3.4
+  },
+  {
+    name: "avocado",
+    weight: 3.4
+  },
+  {
+    name: "banana",
+    weight: 3.4
+  },
+  {
+    name: "cauliflower",
+    weight: 3.4
+  },
+  {
+    name: "lime",
+    weight: 3.4
+  },
+  {
+    name: "peach",
+    weight: 3.4
+  },
+  {
+    name: "mushroom",
+    weight: 3.4
+  },
+  {
+    name: "nectarine",
+    weight: 3.4
+  },
+  {
+    name: "nuts",
+    weight: 3.4
+  },
+  {
+    name: "peanuts",
+    weight: 3.4
+  },
+  {
+    name: "pineapple",
+    weight: 3.4
+  },
+  {
+    name: "pumpkin",
+    weight: 3.4
+  },
+  {
+    name: "peas",
+    weight: 3.4
+  },
+  {
+    name: "cantaloupe",
+    weight: 3.4
+  },
+  {
+    name: "grapes (bunch)",
+    weight: 3.4
+  },
+  {
+    name: "garlic",
+    weight: 3.4
+  },
+  {
+    name: "green bean",
+    weight: 3.4
+  },
+  {
+    name: "guava",
+    weight: 3.4
+  },
+  {
+    name: "jerusalem artichoke",
+    weight: 3.4
+  },
+  {
+    name: "potato",
+    weight: 3.4
+  },
+  {
+    name: "plum",
+    weight: 3.4
+  },
+  {
+    name: "sweet potato",
+    weight: 3.4
+  },
+  {
+    name: "lettuce",
+    weight: 3.4
+  },
+  {
+    name: "strawberry",
+    weight: 3.4
+  },
+  {
+    name: "tomato",
+    weight: 3.4
+  },
+  {
+    name: "turnip",
+    weight: 3.4
+  },
+  {
+    name: "orange",
+    weight: 3.4
+  },
+  {
+    name: "onion",
+    weight: 3.4
+  },
+  {
+    name: "quince",
+    weight: 3.4
+  },
+  {
+    name: "kiwi",
+    weight: 3.4
+  },
+  {
+    name: "leek",
+    weight: 3.4
+  },
+  {
+    name: "pear",
+    weight: 3.4
+  },
+  {
+    name: "mixed greens (assorted)",
+    weight: 3.4
+  },
+  {
+    name: "mango",
+    weight: 3.4
+  },
+  {
+    name: "melon",
+    weight: 3.4
+  },
+  {
+    name: "ugli fruit",
+    weight: 3.4
+  },
+  {
+    name: "vine leaf",
+    weight: 3.4
+  },
+  {
+    name: "victoria plum",
+    weight: 3.4
+  },
+  {
+    name: "cauliflower",
+    weight: 3.4
+  },
+  {
+    name: "watercress",
+    weight: 3.4
+  },
+  {
+    name: "watermelon",
+    weight: 3.4
+  },
+  {
+    name: "yam",
+    weight: 3.4
+  },
+  {
+    name: "zucchini",
+    weight: 3.4
+  }
+
+];
+
 export default fruitsVeggies
